7c295fcd5d53222ce878c0757eabe7db9b1980c6
Lucas Costa <lucasfrct@users.noreply.github.com>
2019-12-07 18:49:47 -0300

    Create main component

---
+++ app/main/main.component.js
@@ -0,0 +1,21 @@
+/*
+ * main.component.js 
+ * Autor: Lucas Costa
+ * Data: Dezewmbro de 2019
+ * */
+
+ ( ( ) => {
+    "use strict";
+
+    angular 
+        .module ( "circuit" )
+        .component ( "main", {
+            templateUrl: "app/main/main.html",
+            controller: [ "$scope", MainController ],
+        } );
+
+    function MainController ( $scope ) {
+        $scope.title = "Main";
+    };
+    
+ } ) ( );
